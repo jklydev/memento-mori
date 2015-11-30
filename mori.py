122d8fc2b418a9cb79d7faba385fdf61a23cf35b
John <jjkiely@gmail.com>
2015-11-30 20:24:28 +0000

    results page with template

--- mori.py
+++ mori.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def mori():
   return render_template('main.html')
+
+@app.route("/results")
+@app.route("/results/<sex>/<age>/")
+def show(sex=None, age=None):
+  return render_template('results.html', sex=sex, age=age)
 
 if __name__ == "__main__":
   app.run()
