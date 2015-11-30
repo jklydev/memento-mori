f36f364c87d63ebabb81eec24016a9e6114fcdbd
John <jjkiely@gmail.com>
2015-11-30 21:12:45 +0000

    Form passing variables

--- mori.py
+++ mori.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def mori():
+  if request.method == 'POST':
+    sex = request.form['sex']
+    age = request.form['age']
+    return redirect(url_for('results', sex=sex, age=age))
+    
   return render_template('main.html')
 
 @app.route("/results")
 @app.route("/results/<sex>/<age>/")
-def show(sex=None, age=None):
+def results(sex=None, age=None):
   return render_template('results.html', sex=sex, age=age)
 
 if __name__ == "__main__":
