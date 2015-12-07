f25e1287f91f60691f24c5ec11ec293e7ad3ecef
John <jjkiely@gmail.com>
2015-12-07 13:53:29 +0000

    make_table function

--- mori.py
+++ mori.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from numpy import genfromtxt
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -15,5 +16,17 @@ def mori():
 def results(sex=None, age=None):
   return render_template('results.html', sex=sex, age=age)
 
+
+def make_table(path):
+    csv = genfromtxt(path, delimiter=",", skip_header=7, skip_footer=3)
+    M = {}
+    F = {}
+    for row in csv:
+        x = row[0]
+        M[x] = { 'q': row[2], 'd': row[4], 'e': row[5] }
+        F[x] = { 'q': row[8], 'd': row[10], 'e': row[11] }
+    return M, F
+
 if __name__ == "__main__":
   app.run()
+
