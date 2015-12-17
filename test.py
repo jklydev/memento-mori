c4a969611e2ab567c788789f6715c75ac91e7a5b
John <jjkiely@gmail.com>
2015-12-17 09:42:05 +0000

    added more tests

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 import unittest
 import mori
 
-class MoriTest(unittest.TestCase):
+class TableTestCase(unittest.TestCase):
+    def setUp(self):
+        self.M, self.F = mori.make_table("tables/2012.csv")
 
+class MoriTest(TableTestCase):
     def test1(self):
-        M, F = mori.make_table("tables/2012.csv")
-        self.assertEqual(M[0]['q'], 0.004722)
+        self.assertEqual(self.M[0]['q'], 0.004722)
+
+    def test2(self):
+        self.assertEqual(self.F[0]['q'], 0.003785)
+
+    def test3(self):
+        self.assertEqual(self.M[100]['d'], 378.2)
+
+    def test4(self):
+        self.assertEqual(self.F[100]['e'], 2.27)
 
 if __name__ == '__main__':
     unittest.main()
