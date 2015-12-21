620feaa19a6e6cbb565262966b75df11a8f63798
John <jjkiely@gmail.com>
2015-12-21 09:53:01 +0000

    created tests for main and results, results failing

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
 import unittest
 import mori
 
-class TableTestCase(unittest.TestCase):
+class TableTest(unittest.TestCase):
     def setUp(self):
         self.M, self.F = mori.make_table("tables/2012.csv")
 
-class MoriTest(TableTestCase):
-    def test1(self):
+    def testMq(self):
         self.assertEqual(self.M[0]['q'], 0.004722)
 
-    def test2(self):
+    def testFq(self):
         self.assertEqual(self.F[0]['q'], 0.003785)
 
-    def test3(self):
+    def testMd(self):
         self.assertEqual(self.M[100]['d'], 378.2)
 
-    def test4(self):
+    def testFe(self):
         self.assertEqual(self.F[100]['e'], 2.27)
+
+class MoriTest(unittest.TestCase):
+    def setUp(self):
+        self.app = mori.app.test_client()
+
+    def testHome(self):
+        home = self.app.get('/')
+        self.assertTrue('Male' in home.data)
+
+    def testResults(self):
+        results = self.app.get('/results')
+        self.assertTrue('Data' in results.data)
 
 if __name__ == '__main__':
     unittest.main()
